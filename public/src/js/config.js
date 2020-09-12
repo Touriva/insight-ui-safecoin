6b2b87d78f4735973796ab9f5ff4618f4654bb42
Martin <m.schillhorn@me.com>
2020-09-12 23:07:28 +0200

    Touriva rebrand

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -5,7 +5,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: 'views/block.html',
-      title: 'Safecoin Block '
+      title: 'Touriva Block '
     }).
     when('/block-index/:blockHeight', {
       controller: 'BlocksController',
@@ -17,7 +17,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: 'views/transaction.html',
-      title: 'Safecoin Transaction '
+      title: 'Touriva Transaction '
     }).
     when('/', {
       templateUrl: 'views/index.html',
@@ -25,15 +25,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: 'views/block_list.html',
-      title: 'Safecoin Blocks solved Today'
+      title: 'Touriva Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: 'views/block_list.html',
-      title: 'Safecoin Blocks solved '
+      title: 'Touriva Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: 'views/address.html',
-      title: 'Safecoin Address '
+      title: 'Touriva Address '
     }).
     when('/charts/:chartType?', {
       templateUrl: 'views/charts.html',
